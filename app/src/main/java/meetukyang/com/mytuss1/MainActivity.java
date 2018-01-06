efebea7db7979fa0d3ef5cc7f8bc01c93342927a
masterUNG <phrombutr@gmail.com>
2018-01-06 16:09:47 +0700

    finish6/1/2018

--- app/src/main/java/meetukyang/com/mytuss1/MainActivity.java
+++ app/src/main/java/meetukyang/com/mytuss1/MainActivity.java
@@ -3,11 +3,22 @@ package meetukyang.com.mytuss1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import meetukyang.com.mytuss1.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+      //  add Fragment to Activitivy
+        if( savedInstanceState==null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.TussMainFragment,new MainFragment())
+                    .commit();
+        }
+
+    } //Main Method
+}// Main Class
---
+++ app/src/main/java/meetukyang/com/mytuss1/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package meetukyang.com.mytuss1.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import meetukyang.com.mytuss1.R;
+
+/**
+ * Created by Chaiwoot on 6/1/2561.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main,container, false);
+        return view;
+    }
+} // Main Class
